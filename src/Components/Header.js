c19953d8584daf5d602457aa20c527efdc993e86
TiffanyJan <tiffany_low@hotmail.com>
2019-01-01 20:24:06 +0000

    components

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+    
+    render(){
+        return (<h1 id="topHeader"> Tiff's To Do List</h1>);
+    }
+
+}
+
+export default Header; 
