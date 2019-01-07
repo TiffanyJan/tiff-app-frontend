98e4cbacf697afd0a994a5e9f715020541ea15f9
TiffanyJan <tiffany_low@hotmail.com>
2019-01-07 21:01:34 +0000

    event binding function added

--- src/Components/TaskEntry.js
+++ src/Components/TaskEntry.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
 class TaskEntry extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state={
+            taskDescription: ""
+        };
+
+      
+        this.onSaveClicked = this.onSaveClicked.bind(this);
+        this.onTaskTestFieldUpdated = this.onTaskTestFieldUpdated.bind(this);
+    }
+
+    onSaveClicked() {
+        alert("New Task Saved!");
+    }
+
+    onTaskTestFieldUpdated(event) {
+        const description = event.target.value;
+
+        this.setState({
+            taskDescription: description
+        });
+    }
+
     render(){
         return (
             <div style={styles.header} className="row">
                 <div className="col">
-                    <input type="text" value="" />
+                    <input type="text" value={this.state.taskDescription} onChange={this.onTaskTestFieldUpdated} />
                 </div>
                 <div className="col">
-                    <input type="button" value="Save" />
+                    <input type="button" value="Save" onClick={this.onSaveClicked} />
                 </div>
             </div>
         );
